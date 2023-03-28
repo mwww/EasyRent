174d4d70b493db92ac1761ffb478246066050ce6
mwww dev <115621609+mwww@users.noreply.github.com>
2023-03-28 20:37:12 +0700

    Update index.tsx

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,17 @@
+// Components
+import NavBar from '../../components/navbar/navbar'
+
+// Pages
 import Hero from './hero/hero'
 import Main from './main/main'
 
+// Style
 import style from './index.module.scss'
 
 export default function Index() {
   return (
     <>
+      <NavBar />
       <Hero />
       <Main />
     </>
