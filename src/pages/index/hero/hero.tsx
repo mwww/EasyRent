b425fe30f295298d4aaa71a788277f306c2b2431
mwww dev <115621609+mwww@users.noreply.github.com>
2023-03-28 20:37:00 +0700

    redesign hero

--- src/pages/index/hero/hero.tsx
+++ src/pages/index/hero/hero.tsx
@@ -1,3 +1,16 @@
+import css from './hero.module.scss'
+
 export default function Hero() {
-  return <header className="content_wrapper">Hero!</header>
+  return (
+    <header className={css.hero}>
+      <div className={`content_wrapper ${css.wrap}`}>
+        <div>
+          <div className={`background_def ${css.bg}`}></div>
+        </div>
+        <div className={css.tagline}>
+          Rent Any Cars The <span>Easy Way!</span>
+        </div>
+      </div>
+    </header>
+  )
 }
