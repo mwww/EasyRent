06864b6f199d5e39d40a10354cc20059f9b72bad
mwww dev <115621609+mwww@users.noreply.github.com>
2023-03-28 20:36:31 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router'
 import './base.scss'
+import './g-font.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
