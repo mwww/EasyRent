0039aa1b89963bdfc6cf9fa50478732f0dd0c70a
mwww dev <115621609+mwww@users.noreply.github.com>
2023-03-29 22:56:34 +0700

    Update main.tsx

--- src/pages/index/main/main.tsx
+++ src/pages/index/main/main.tsx
@@ -1,5 +1,13 @@
+// Components
+import Superiority from './superiority/superiority'
+
+// Style
 import style from './main.module.scss'
 
 export default function Main() {
-  return <main className={`content_wrapper ${style.main} `}>main</main>
+  return (
+    <main className={`content_wrapper ${style.main} `}>
+      <Superiority />
+    </main>
+  )
 }
