da51e380ca488312dcfda11f4b852399f90a5f0c
mwww dev <115621609+mwww@users.noreply.github.com>
2023-03-31 21:19:22 +0700

    make CTA undragable

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
             </li>
           ))}
         </ul>
-        <Link to="">
+        <Link to="" draggable="false">
           <button>Rent a Car Now!</button>
         </Link>
       </div>
