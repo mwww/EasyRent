f713843a526fce97a5d951a07cea596eb41347fb
mwww dev <115621609+mwww@users.noreply.github.com>
2023-04-01 23:26:14 +0700

    designed featured cars

--- src/pages/index/main/featured/featured.tsx
+++ src/pages/index/main/featured/featured.tsx
@@ -1,8 +1,34 @@
+import css from './featured.module.scss'
+
 export default function Featured() {
   return (
-    <section>
-      <div>image here</div>
-      <div>content here</div>
+    <section className={css.featured}>
+      {/* <div>image here</div> */}
+      <div className="background_def"></div>
+      <div className={css.description}>
+        <div>
+          <h3>Porsche 911</h3>
+          <p>Porsche</p>
+        </div>
+        <div>
+          <p>8-speed AT, 7-speed MT.</p>
+          <p>3.0-liter twin-turbocharged flat-six.</p>
+          <p>572 hp, 553 lb-ft.</p>
+        </div>
+        <div className={css.action}>
+          <a href="" draggable="false">
+            <button>Rent Now!</button>
+          </a>
+          <div>
+            <a href="">
+              <button>&lArr;</button>
+            </a>
+            <a href="">
+              <button>&rArr;</button>
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
