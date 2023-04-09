2f91c49220640b6346e51425b2f972904bc09243
mwww dev <115621609+mwww@users.noreply.github.com>
2023-04-09 23:54:38 +0700

    created: actionCard component

--- src/pages/index/main/actionCard/actionCard.tsx
+++ src/pages/index/main/actionCard/actionCard.tsx
@@ -1,3 +1,13 @@
+import css from './actionCard.module.scss'
+
 export default function ActionCard() {
-  return <p>action card!</p>
+  return (
+    <section className={css.actionCard}>
+      <div className={css.blurGrad}></div>
+      <div className={css.texts}>
+        Click <a href="">here</a> when you're ready to rent a car!
+        <h3></h3>
+      </div>
+    </section>
+  )
 }
