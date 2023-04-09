7e2725808577c3bba2f2eae0559b924ccc6b1a82
mwww dev <115621609+mwww@users.noreply.github.com>
2023-04-09 23:53:14 +0700

    added: /catalogue route

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 // Pages
 import Index from './pages/index/index'
+import Catalogue from './pages/catalogue/catalogue'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Index />,
+  },
+  {
+    path: '/catalogue',
+    element: <Catalogue />,
   },
 ])
 
