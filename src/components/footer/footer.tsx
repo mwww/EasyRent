ca354e92f71f31d599cf93e809f0b05a40245625
mwww dev <115621609+mwww@users.noreply.github.com>
2023-04-10 16:40:00 +0700

    updated: contain links in an array

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,71 @@
 import css from './footer.module.scss'
 
 export default function Footer() {
+  const footerLinks = [
+    {
+      Title: 'SiteMap',
+      Links: [
+        {
+          Text: 'Home',
+          Link: '/',
+        },
+        {
+          Text: 'Catalogue',
+          Link: '/catalogue',
+        },
+        {
+          Text: 'About Us',
+          Link: '/aboutus',
+        },
+        {
+          Text: 'Contact',
+          Link: '/contact',
+        },
+      ],
+    },
+    {
+      Title: 'Costumer',
+      Links: [
+        {
+          Text: 'FAQ',
+          Link: '/',
+        },
+        {
+          Text: 'Policy',
+          Link: '/',
+        },
+        {
+          Text: 'Support',
+          Link: '/',
+        },
+        {
+          Text: 'Feedback',
+          Link: '/',
+        },
+      ],
+    },
+    {
+      Title: 'Company',
+      Links: [
+        {
+          Text: 'Our Story',
+          Link: '/',
+        },
+        {
+          Text: 'Members',
+          Link: '/',
+        },
+        {
+          Text: 'Careers',
+          Link: '/',
+        },
+        {
+          Text: 'Collaboration',
+          Link: '/',
+        },
+      ],
+    },
+  ]
   return (
     <footer className={css.footer}>
       <div className={`content_wrapper`}>
@@ -11,71 +76,20 @@ export default function Footer() {
             <div className={css.logoContainer}>
               <a href="" className={`background_def ${css.logo}`}></a>
             </div>
-            {/* <p>
-              EasyRent is a platform to rent awesome cars, dont care its for
-              music video or something or something
-            </p> */}
-            {/* <p>
-              EasyRent offers high-quality, hassle-free car rental with flexible
-              terms and exceptional service. Find the perfect car for any
-              occasion, from music video shoots to daily use, all at competitive
-              prices. With 24/7 support, EasyRent is the ultimate car rental web
-              app.
-            </p> */}
           </div>
-          <div>
+
+          {footerLinks.map((cat) => (
             <div>
-              <h3>SiteMap</h3>
+              <h3>{cat.Title}</h3>
               <ul>
-                <li>
-                  <a href="">Home</a>
-                </li>
-                <li>
-                  <a href="">Catalogue</a>
-                </li>
-                <li>
-                  <a href="">About Us</a>
-                </li>
-                <li>
-                  <a href="">Contact</a>
-                </li>
+                {cat.Links.map((link) => (
+                  <li>
+                    <a href={link.Link}>{link.Text}</a>
+                  </li>
+                ))}
               </ul>
             </div>
-          </div>
-          <div>
-            <h3>Costumer</h3>
-            <ul>
-              <li>
-                <a href="">FAQ</a>
-              </li>
-              <li>
-                <a href="">Policy</a>
-              </li>
-              <li>
-                <a href="">Support</a>
-              </li>
-              <li>
-                <a href="">Feedback</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3>Company</h3>
-            <ul>
-              <li>
-                <a href="">Our Story</a>
-              </li>
-              <li>
-                <a href="">Members</a>
-              </li>
-              <li>
-                <a href="">Careers</a>
-              </li>
-              <li>
-                <a href="">Collaboration</a>
-              </li>
-            </ul>
-          </div>
+          ))}
           <div>
             <h3>Find us at</h3>
             <ul>
@@ -91,7 +105,10 @@ export default function Footer() {
             <ul className={css.socialIcons}>
               <li>
                 <a href="">
-                  <img src="" alt="fb" />
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg"
+                    alt="fb"
+                  />
                 </a>
               </li>
               <li>
