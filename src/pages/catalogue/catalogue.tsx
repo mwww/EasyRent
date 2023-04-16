35d8d93f15bd937433e272b87ef8b8266ecc1878
mwww dev <115621609+mwww@users.noreply.github.com>
2023-04-16 23:52:55 +0700

    added: window title and implement components

--- src/pages/catalogue/catalogue.tsx
+++ src/pages/catalogue/catalogue.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from 'react'
+
+// External Components
+import UniversalHero from '../../components/universalHero/universalHero'
+import NavBar from '../../components/navbar/navbar'
+import Footer from '../../components/footer/footer'
+
+// internal Components
+import Main from './main/main'
+
 export default function Catalogue() {
-  return <h1>Catalogue page!</h1>
+  useEffect(() => {
+    document.title = 'Catalogue - EasyRent'
+  }, [])
+  return (
+    <>
+      <NavBar />
+      <UniversalHero text="Catalogue" />
+      <Main />
+      <Footer />
+    </>
+  )
 }
