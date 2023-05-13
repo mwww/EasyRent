80c34cc22550c657e16fd3605ff618988e052768
Rizki0720 <rizkigunawan7@gmail.com>
2023-05-13 19:49:28 +0700

    create tabel

--- src/pages/app/dashboard/main/main.tsx
+++ src/pages/app/dashboard/main/main.tsx
@@ -1,6 +1,7 @@
 import SideBar from '../SideBar/sidebar'
 import css from './main.module.scss'
 import React,{useState,useEffect} from 'react'
+import Tabel from './tabel/tabel'
 
 
 function Main() {
@@ -12,40 +13,8 @@ function Main() {
         <main className={`${css.bg} background_def`}>
             <div className={css.container}>
                <SideBar/>
-                <div className={css.boxtable}>
-                  <table className={css.table}>
-                    <thead>
-                      <tr>
-                        <th>Nomor </th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Nama Mahasiswa</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Ayu Daini</td>
-                        <td>Sistem Informasi</td>
-                      </tr>
-
-                      <tr>
-                        <td>2</td>
-                        <td>Islahudin</td>
-                        <td>Teknik Informatika</td>
-                      </tr>
-
-                      <tr>
-                        <td>3</td>
-                        <td>Gufron Hasan</td>
-                        <td>Teknik Informatika</td>
-                      </tr>
-
-                  </tbody>
-                  </table>
-                </div> 
+                
+                <Tabel/> 
             </div>
         </main>
     </div>
