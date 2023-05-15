c0a93ea9f5cc9e8d935381a68d7edc0fc8f20022
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-16 01:56:16 +0700

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Pages
 import Index from './pages/index/index'
@@ -6,49 +6,23 @@ import DummyPage from './pages/dummyPage/dummyPage'
 import Catalogue from './pages/catalogue/catalogue'
 import AboutUs from './pages/aboutUs/aboutUs'
 import Checkout from './pages/checkout/checkout'
-import Login from './pages/app/login/login';
+import Login from './pages/app/login/login'
 import Dashboard from './pages/app/dashboard/dashboard'
-import Register from './pages/app/register/register';
-import { element } from 'prop-types'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Index />,
-  },
-
-  {
-    path: '/dummy-page',
-    element: <DummyPage />,
-  },
-
-  {
-    path: '/catalogue',
-    element: <Catalogue />,
-  },
-  {
-    path: '/AboutUs',
-    element: <AboutUs />,
-  },
-
-  {
-    path: '/checkout',
-    element: <Checkout />,
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/admin',
-    element: <Dashboard />
-  },
-  {
-    path: '/register',
-    element: <Register />
-  },
-])
+import Register from './pages/app/register/register'
 
 export default function Router() {
-  return <RouterProvider router={router} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/dummy-page" element={<DummyPage />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
