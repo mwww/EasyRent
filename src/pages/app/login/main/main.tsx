f3267d2a9ae69631bee928e433c06b7e387102df
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-16 04:12:03 +0700

    Update main.tsx

--- src/pages/app/login/main/main.tsx
+++ src/pages/app/login/main/main.tsx
@@ -23,35 +23,6 @@ const main = () => {
       }
     }
   }
-
-  // return (
-  //   <main className={`${css.bg} background_def`}>
-  //     <div className={css.container}>
-  //       <div className={css.form}>
-  //         <h1>Welcome</h1>
-  //         <p>Please enter your details.</p>
-  //         <form className="" onSubmit={auth}>
-  //           <label htmlFor="Email"></label>
-  //           <input
-  //             type="text"
-  //             placeholder="Email"
-  //             value={email}
-  //             onChange={(e) => setEmail(e.target.value)}
-  //           />
-  //           <label htmlFor="Password"></label>
-  //           <input
-  //             type="password"
-  //             placeholder="Password"
-  //             value={password}
-  //             onChange={(e) => setPassword(e.target.value)}
-  //           />
-  //           <div className={css.btn}>
-  //             <button>Log In</button>
-  //           </div>
-  //         </form>
-  //         <div className={css.register}>
-  //           <p>Don't have an account ?</p>
-  //           <Link to={'/register'}>
   return (
     <main className={`${css.main} background_def`}>
       <div className={css.container}>
