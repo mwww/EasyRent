ff2ff09e1a696ffeeebad023b68e847b44db2639
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-16 04:13:30 +0700

    Update login.tsx

--- src/pages/app/login/login.tsx
+++ src/pages/app/login/login.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import UniversalHero from '../../../components/universalHero/universalHero';
 import NavBar from '../../../components/navbar/navbar'
-import Footer from '../../../components/footer/footer'
-import Main from './main/main';
+import Main from './main/main'
 
 const login = () => {
+  useEffect(() => {
+    document.title = 'Login - EasyRent'
+  }, [])
   return (
     <>
-        <NavBar />
-        {/* <UniversalHero text='Login Pages'/> */}
-        <Main />
-        {/* <Footer /> */}
+      <NavBar />
+      <Main />
     </>
   )
 }
