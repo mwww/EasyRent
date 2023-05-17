1cd2c22342cae73c78980e26f810005b93978a87
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-17 23:28:22 +0700

    Create main.tsx

---
+++ src/pages/e404/main/main.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main className={`content_wrapper`}>
+      <h3>This page is unavailable!</h3>
+      <p>
+        I'm sorry for the inconvenience, but it looks like we didn't have what
+        you were asking for.
+        <br />
+        however, you can go back home if you want to!
+      </p>
+    </main>
+  )
+}
