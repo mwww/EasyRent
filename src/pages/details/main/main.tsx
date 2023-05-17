1788a4e668c991164435959e5bd4ed14c4d53fda
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-17 23:28:16 +0700

    Create main.tsx

---
+++ src/pages/details/main/main.tsx
@@ -0,0 +1,11 @@
+export default function Main() {
+  return (
+    <main className={`content_wrapper`}>
+      <h3>Oh, Hi!</h3>
+      <p>
+        I think the page you're looking for is snot made yet, so we use this as
+        a place holder. Hope you don't get mad!
+      </p>
+    </main>
+  )
+}
