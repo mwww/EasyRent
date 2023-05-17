4b059d2567aba6be878b90990818649888e7271a
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-17 23:28:27 +0700

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // Pages
 import Index from './pages/index/index'
@@ -6,9 +6,14 @@ import DummyPage from './pages/dummyPage/dummyPage'
 import Catalogue from './pages/catalogue/catalogue'
 import AboutUs from './pages/aboutUs/aboutUs'
 import Checkout from './pages/checkout/checkout'
+import Details from './pages/details/details'
+
 import Login from './pages/app/login/login'
-import Dashboard from './pages/app/dashboard/dashboard'
 import Register from './pages/app/register/register'
+
+import Dashboard from './pages/app/dashboard/dashboard'
+
+import E404 from './pages/e404/e404'
 
 export default function Router() {
   return (
@@ -19,9 +24,17 @@ export default function Router() {
         <Route path="/catalogue" element={<Catalogue />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/details" element={<Navigate to="/catalogue" replace />} />
+        <Route path="/details/:id" element={<Details />} />
+
+        {/* auth */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* management */}
         <Route path="/admin" element={<Dashboard />} />
+
+        <Route path="*" element={<E404 />} />
       </Routes>
     </BrowserRouter>
   )
