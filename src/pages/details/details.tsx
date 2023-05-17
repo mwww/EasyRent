55a83be5b7097d853941b7b4b00d4931d8cda94b
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-18 03:53:43 +0700

    Update details.tsx

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 // External Components
 import UniversalHero from '../../components/universalHero/universalHero'
@@ -6,20 +7,81 @@ import NavBar from '../../components/navbar/navbar'
 import Footer from '../../components/footer/footer'
 
 // internal Components
+import Bg from './bg/bg'
 import Main from './main/main'
-import { useParams } from 'react-router-dom'
+
+interface CarData {
+  id_mobil: number
+  popularity_idx: number
+  model: string
+  brand: string
+  release_year: number
+  price: number
+  engine: string
+  HP: number
+  TRQ: number
+  transmissions: Transmission[]
+  images: string[]
+}
+
+interface Transmission {
+  transmission_type: string
+  speed: number
+}
 
 export default function Details() {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [carData, setCarData] = useState<CarData>()
+  const [e, setE] = useState<boolean>(false)
   const params = useParams()
+
   useEffect(() => {
-    document.title = 'Detail - EasyRent'
+    async function fetchData() {
+      setIsLoading(true)
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/car/${params.id}`
+        )
+        const res = (await response.json()).data
+
+        console.log(res)
+        if (res) {
+          setCarData(res)
+          setIsLoading(false)
+        } else setE(true)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
   }, [])
-  return (
-    <>
-      <NavBar />
-      <UniversalHero text={`${JSON.stringify(params)}`} />
-      <Main />
-      <Footer />
-    </>
-  )
+
+  useEffect(() => {
+    document.title = (isLoading ? 'Detail' : carData!.model) + ' - EasyRent'
+  }, [])
+
+  if (carData && !e) {
+    return (
+      <>
+        <NavBar />
+        <UniversalHero
+          text={isLoading ? 'loading...' : `${carData.brand} ${carData.model}`}
+        />
+        {/* <p>{JSON.stringify(carData)}</p> */}
+        {/* <Bg imgUrl={isLoading ? '' : carData.images[0]} /> */}
+        <Main carData={carData} />
+        <Footer />
+      </>
+    )
+  } else if (!carData && e) {
+    return (
+      <>
+        <NavBar />
+        <UniversalHero text={`Error`} />
+        <Footer />
+      </>
+    )
+  }
 }
+
+export type { CarData }
