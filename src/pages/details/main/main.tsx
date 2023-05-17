7baa223c0ba53fe6882f37c44aa550af8af53d52
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-18 04:30:57 +0700

    Update main.tsx

--- src/pages/details/main/main.tsx
+++ src/pages/details/main/main.tsx
@@ -21,11 +21,6 @@ export default function Main(props: Props) {
   }
   return (
     <main className={`content_wrapper`}>
-      {/* <h3>Oh, Hi!</h3>
-      <p>
-        I think the page you're looking for is snot made yet, so we use this as
-        a place holder. Hope you don't get mad!
-      </p> */}
       <Bg imgUrl="" />
       <div className={css.car_detail}>
         <div></div>
