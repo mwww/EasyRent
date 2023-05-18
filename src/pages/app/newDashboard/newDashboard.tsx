0cf9e08c5a822c9936d612bf4fdd132fe138c532
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-18 20:20:24 +0700

    Create newDashboard.tsx

---
+++ src/pages/app/newDashboard/newDashboard.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+import css from './newDashboard.module.scss'
+
+export default function NewDashboard() {
+  useEffect(() => {
+    document.title = 'New Dashboard - EasyRent'
+  }, [])
+  return (
+    <div className={css.container}>
+      <aside>
+        <div>
+          <div className={css.logo_container}>
+            <Link to="" className={`background_def ${css.logo}`}></Link>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="overview">Overview</Link>
+              </li>
+              <li>
+                <Link to="cars">Cars</Link>
+              </li>
+              <li>
+                <Link to="appointments">Appointments</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <div></div>
+          <p>nama user</p>
+        </div>
+      </aside>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
