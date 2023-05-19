6b88e7f0ce132c4bdc38b5b510a5fd91306f880a
Rizki0720 <rizkigunawan7@gmail.com>
2023-05-20 00:09:34 +0700

    menambah gambar pada folder public

--- src/pages/app/newDashboard/sub/cars/cars.tsx
+++ src/pages/app/newDashboard/sub/cars/cars.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 
 import css from './cars.module.scss'
 
-import Confirmation from './modals/deleteCar/deleteCar'
-
 interface CarData {
   id_mobil: number
   popularity_idx: number
@@ -24,30 +22,27 @@ export default function Cars() {
     direction: 'asc' | 'desc'
   }>({ column: '', direction: 'asc' })
   const [searchQuery, setSearchQuery] = useState('')
-  const [modal, setModal] = useState<React.ReactNode>(null)
 
   useEffect(() => {
-    document.title = 'Cars - EasyRent'
+    document.title = 'Overview - EasyRent'
   }, [])
 
   useEffect(() => {
-    fetchAndSetData()
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:3000/admin/cars`)
+        const { data } = await response.json()
+        setCarsData(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
   }, [])
 
   useEffect(() => {
     setTableData(carsData)
   }, [carsData])
-
-  const fetchAndSetData = () => {
-    fetch(`http://localhost:3000/admin/cars`)
-      .then(async (response) => {
-        const { data } = await response.json()
-        setCarsData(data)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-      })
-  }
 
   const handleSort = (column: string) => {
     setSorting((prevSorting) => ({
@@ -60,10 +55,6 @@ export default function Cars() {
           : 'asc',
     }))
     setSearchQuery('')
-  }
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
   }
 
   const filteredData = searchQuery
@@ -82,23 +73,14 @@ export default function Cars() {
     return direction === 'asc' ? aValue - bValue : bValue - aValue
   })
 
-  const deleteCar = (id: number) => {
-    const confirmationComponent = <Confirmation id={id} onClose={modalClose} />
-    setModal(confirmationComponent)
-  }
-
-  const modalClose = (isDeleted: boolean) => {
-    setModal(null)
-    if (isDeleted) {
-      fetchAndSetData()
-    }
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
   }
 
   return (
     <>
       {/* ========== */}
       {/* modal here */}
-      {modal}
       {/* ========== */}
       <div className={css.container}>
         <div>
@@ -107,18 +89,7 @@ export default function Cars() {
             <p>{sortedData.length} cars found!</p>
           </div>
           <div>
-            <p>
-              menu (add car, sorting)
-              <a
-                href=""
-                onClick={(e) => {
-                  e.preventDefault()
-                  fetchAndSetData()
-                }}
-              >
-                Refresh
-              </a>
-            </p>
+            <p>menu (add car, sorting)</p>
             <input
               type="text"
               value={searchQuery}
@@ -128,57 +99,40 @@ export default function Cars() {
           </div>
         </div>
         <div className={css.table_container}>
-          {sortedData.length > 0 ? (
-            <table className={css.table}>
-              <thead>
-                <tr>
-                  <th onClick={() => handleSort('id_mobil')}>ID</th>
-                  <th onClick={() => handleSort('popularity_idx')}>
-                    Popularity Index
-                  </th>
-                  <th onClick={() => handleSort('model')}>Model</th>
-                  <th onClick={() => handleSort('brand')}>Brand</th>
-                  <th onClick={() => handleSort('release_year')}>
-                    Release Year
-                  </th>
-                  <th onClick={() => handleSort('price')}>Price</th>
-                  <th onClick={() => handleSort('engine')}>Engine</th>
-                  <th onClick={() => handleSort('HP')}>HP</th>
-                  <th onClick={() => handleSort('TRQ')}>TRQ</th>
-                  <th>Actions</th>
+          <table className={css.table}>
+            <thead>
+              <tr>
+                <th onClick={() => handleSort('id_mobil')}>ID</th>
+                <th onClick={() => handleSort('popularity_idx')}>
+                  Popularity Index
+                </th>
+                <th onClick={() => handleSort('model')}>Model</th>
+                <th onClick={() => handleSort('brand')}>Brand</th>
+                <th onClick={() => handleSort('release_year')}>Release Year</th>
+                <th onClick={() => handleSort('price')}>Price</th>
+                <th onClick={() => handleSort('engine')}>Engine</th>
+                <th onClick={() => handleSort('HP')}>HP</th>
+                <th onClick={() => handleSort('TRQ')}>TRQ</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedData.map((row, index) => (
+                <tr key={index}>
+                  <td>{row.id_mobil}</td>
+                  <td>{row.popularity_idx}</td>
+                  <td>{row.model}</td>
+                  <td>{row.brand}</td>
+                  <td>{row.release_year}</td>
+                  <td>{row.price}</td>
+                  <td>{row.engine}</td>
+                  <td>{row.HP}</td>
+                  <td>{row.TRQ}</td>
+                  <td>del, edit</td>
                 </tr>
-              </thead>
-              <tbody>
-                {sortedData.map((row, index) => (
-                  <tr key={index}>
-                    <td>{row.id_mobil}</td>
-                    <td>{row.popularity_idx}</td>
-                    <td>{row.model}</td>
-                    <td>{row.brand}</td>
-                    <td>{row.release_year}</td>
-                    <td>{row.price}</td>
-                    <td>{row.engine}</td>
-                    <td>{row.HP}</td>
-                    <td>{row.TRQ}</td>
-                    <td>
-                      <a
-                        href="/"
-                        onClick={(e) => {
-                          e.preventDefault()
-                          deleteCar(row.id_mobil)
-                        }}
-                      >
-                        del
-                      </a>
-                      , edit
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          ) : (
-            <p>No data found!</p>
-          )}
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
