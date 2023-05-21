cc317777dd600a2d35ab8655bd5dd96a506b69e9
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-21 08:54:44 +0700

    added background image and detail image

--- src/pages/details/main/main.tsx
+++ src/pages/details/main/main.tsx
@@ -9,6 +9,8 @@ interface Props {
   carData: CarData
 }
 
+// const
+
 export default function Main(props: Props) {
   const carData = props.carData
   if (!carData) {
@@ -20,17 +22,37 @@ export default function Main(props: Props) {
       </main>
     )
   }
+  const toCurrency = (x: any) => {
+    return `Rp. ${x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+  }
   return (
     <main className={`content_wrapper ${css.main}`}>
-      <Bg imgUrl="" />
-      <div className={css.car_detail}>
-        <div></div>
+      <Bg imgUrl={`${carData.images[0]}`} />
+      <section className={css.car_detail}>
+        <div
+          className="background_def"
+          style={{ backgroundImage: `url("/imgs/cars/${carData.images[0]}")` }}
+        ></div>
         <div>
           <h3>{carData.model}</h3>
           <p>{carData.brand}</p>
+          {/* <p>{JSON.stringify(carData)}</p> */}
+          <div>
+            <p>
+              {carData.transmissions.map(
+                (t) =>
+                  `${t.speed}-Speed ${
+                    t.transmission_type === 'AT' ? 'Auto' : 'Manual'
+                  }`
+              )}
+            </p>
+            <p>{carData.engine}.</p>
+            <p>{`${carData.HP} hp, ${carData.TRQ} lb-ft.`}</p>
+          </div>
+          <h3>{toCurrency(carData.price)}</h3>
         </div>
-      </div>
-      <Appointment />
+      </section>
+      <Appointment id={carData.id_mobil} />
     </main>
   )
 }
